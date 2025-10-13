0b5d69fb99322d355ad4420d5187619f626124ff
Luis Enrique Gil Moya <luisenriquegilmoya@MacBook-Pro-de-Luis.local>
2025-10-13 15:08:48 -0300

    chore(logistica): rename 0012 (sin tilde) y corrige dependencia en 0013

--- logistica/migrations/0013_material_valor_unitario.py
+++ logistica/migrations/0013_material_valor_unitario.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('logistica', '0012_salidamaterial_fecha_emision'),
+        ('logistica', '0012_salidamaterial_fecha_emisión'),
     ]
 
     operations = [
